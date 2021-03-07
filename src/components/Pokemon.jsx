d9876f8828bfe3edaa86ce02e6b02ab7049b01cf
stephenPC <stephenmissah@outlook.com>
2021-03-07 12:30:11 +0000

    destrusted and renamed results from gql

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -11,11 +11,19 @@ const Pokemon = ({ pokemon }) => {
       fast: { name: fastAttackName, damage: fastAttackDamage },
       special: { name: specialAttackName, damage: specialAttackDamage },
     },
-    // evolutions,
+    evolutions,
   } = pokemon;
   return (
-    <div>
-      <img src={image} alt="" />
+    <div className="pokemon">
+      <div className="pokemon__name">
+        <p>{name}</p>
+      </div>
+      <div className="pokemon__meta">max weight, hp and cp</div>
+      <div className="pokemon__image">image</div>
+      <div className="pokemon__attacks">
+        <div className="pokemon__attacksFast">fast attacks name and dmg</div>
+        <div className="pokemon__attacksSpecial">special attacks and dmg</div>
+      </div>
     </div>
   );
 };
